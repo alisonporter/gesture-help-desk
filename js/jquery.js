fd7414e85fc25392157bd3c0cdc41a510d8c9154
alisonporter <ABC@Alisons-MacBook-Air.local>
2015-09-02 13:54:54 -0400

    added jQuery to drop down menu, updated "pages"

--- js/jquery.js
+++ js/jquery.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
 
-  function() {
-    $("ul").slideDown("fast");
-  }
+  var top_nav = $("#top-nav");
+  var toggle = 1;
 
-
-  $(".menuIcon").click(showNav)
+  $(".menuIcon").on('click', function() {
+    if (toggle) {
+      top_nav.slideDown("fast");
+      toggle = 0;
+    } else {
+      top_nav.slideUp("fast");
+      toggle = 1;
+    }
+  });
 
 
 
